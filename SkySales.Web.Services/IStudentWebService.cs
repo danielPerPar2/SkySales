e2eee6ac936fe380e0ee055e0e32062bea5fdce2
Daniel Pérez <daniel.perezpa@hotmail.es>
2019-06-29 20:38:30 +0200

    Added StudentWebService scafolding

---
+++ SkySales.Web.Services/IStudentWebService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace SkySales.Web.Services
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IStudentWebService
+    {
+        [OperationContract]
+        Student Add(Student student);
+
+        [OperationContract]
+        Student GetById(int id);
+
+        [OperationContract]
+        List<Student> GetAll();
+
+        [OperationContract]
+        Student Update(Student student);
+
+        [OperationContract]
+        Student Delete(int id);      
+    }
+
+    [DataContract]
+    public class Student
+    {        
+        [DataMember]
+        public int StudentId{ get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string Surname { get; set; }
+
+        [DataMember]
+        public int Age { get; set; }
+    }
+}
---
+++ SkySales.Web.Services/StudentWebService.svc.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+
+namespace SkySales.Web.Services
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class StudentWebService : IStudentWebService
+    {
+        //Business object del student
+        //private IStudentBO studentBO = new StudentBO();
+        public Student Add(Student student)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Student Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Student> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Student GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Student Update(Student student)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
