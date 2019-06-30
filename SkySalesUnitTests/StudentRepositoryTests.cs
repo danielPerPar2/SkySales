60e5995688786330a4deb7a99e5cacaf1cc089fb
Daniel Pérez <daniel.perezpa@hotmail.es>
2019-06-30 16:52:53 +0200

    Fix in UnitTests

--- SkySalesUnitTests/StudentRepositoryTests.cs
+++ SkySalesUnitTests/StudentRepositoryTests.cs
@@ -21,7 +21,7 @@ namespace SkySalesUnitTests
             mock.Setup(x => x.Add(It.IsAny<Student>())).Returns<Student>(x => x);               
             mock.Setup(x => x.GetById(It.IsAny<Int32>())).Returns(new Student());
             mock.Setup(x => x.GetAll()).Returns(new List<Student>());
-            mock.Setup(x => x.Update(It.IsAny<Int32>())).Returns(new Student());
+            mock.Setup(x => x.Update(It.IsAny<Student>())).Returns(new Student());
             mock.Setup(x => x.Delete(It.IsAny<Int32>())).Returns(new Student());
 
             mockObject = mock.Object;
@@ -52,7 +52,8 @@ namespace SkySalesUnitTests
         [TestMethod]
         public void UpdateTest()
         {
-            var result = mockObject.Update(0);
+            Student student = new Student(1, "Test", "Test", 10);
+            var result = mockObject.Update(student);
             Assert.IsInstanceOfType(result, typeof(Student));
         }
 
